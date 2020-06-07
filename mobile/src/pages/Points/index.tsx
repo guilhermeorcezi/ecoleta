b18f7f357a87964e53fa6ae51eaa7f24306c0b26
Guilherme Orcezi <guilhermeorcezi@gmail.com>
2020-06-07 11:14:08 -0300

    image upload updated

--- mobile/src/pages/Points/index.tsx
+++ mobile/src/pages/Points/index.tsx
@@ -27,6 +27,7 @@ interface Point {
 	id: number;
 	name: string;
 	image: string;
+	image_url: string;
 	latitude: number;
 	longitude: number;
 }
@@ -150,7 +151,7 @@ const Points = () => {
 									<View style={styles.mapMarkerContainer}>
 										<Image
 											source={{
-												uri: point.image,
+												uri: point.image_url,
 											}}
 											style={styles.mapMarkerImage}
 										/>
