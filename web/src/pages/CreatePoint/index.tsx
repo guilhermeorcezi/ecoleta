8603c1de392af8dbd427688acf8c44d02e9935bb
Guilherme Orcezi <guilhermeorcezi@gmail.com>
2020-06-07 11:46:39 -0300

    dropFile added

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -10,6 +10,8 @@ import { useHistory } from 'react-router-dom';
 import logo from '../../assets/logo.svg';
 import { toast } from 'react-toastify';
 
+import Dropzone from '../../components/Dropzone';
+
 const CreatePoint = () => {
 	const [items, setItems] = useState<Item[]>([]);
 	const [ufs, setUfs] = useState<string[]>([]);
@@ -34,6 +36,7 @@ const CreatePoint = () => {
 		0,
 		0,
 	]);
+	const [selectedFile, setSelectedFile] = useState<File>();
 
 	interface Item {
 		id: number;
@@ -131,16 +134,17 @@ const CreatePoint = () => {
 		const [latitude, longitude] = selectedPosition;
 		const items = selectedItems;
 
-		const data = {
-			name,
-			email,
-			whatsapp,
-			uf,
-			city,
-			latitude,
-			longitude,
-			items,
-		};
+		const data = new FormData();
+		data.append('name', name);
+		data.append('email', email);
+		data.append('whatsapp', whatsapp);
+		data.append('uf', uf);
+		data.append('city', city);
+		data.append('latitude', String(latitude));
+		data.append('longitude', String(longitude));
+		data.append('items', items.join(','));
+
+		if (selectedFile) data.append('image', selectedFile);
 
 		try {
 			await api.post('points', data);
@@ -166,6 +170,8 @@ const CreatePoint = () => {
 				<h1>
 					Cadastro do <br /> ponto de coleta.
 				</h1>
+
+				<Dropzone onFileUploaded={setSelectedFile} />
 
 				<fieldset>
 					<legend>
