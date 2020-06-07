76bc3a1f2fd2d04ed5be8c179d624d0fe735bb42
Guilherme Orcezi <guilhermeorcezi@gmail.com>
2020-06-07 11:15:42 -0300

    multer config updated

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -17,7 +17,7 @@ class PointsController {
 		const trx = await knex.transaction();
 
 		const point = {
-			image: 'https://images.unsplash.com/photo-1556767576-5ec41e3239ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
+			image: req.file.filename,
 			name,
 			email,
 			whatsapp,
@@ -31,9 +31,11 @@ class PointsController {
 
 		const point_id = insertedIds[0];
 
-		const pointItems = items.map((item_id: number) => {
-			return { item_id, point_id };
-		});
+		const pointItems = items.split(',')
+			.map((item: string) => Number(item.trim()))
+			.map((item_id: number) => {
+				return { item_id, point_id };
+			});
 
 		await trx('points_item').insert(pointItems);
 
@@ -56,7 +58,12 @@ class PointsController {
 			.where('points_item.point_id', id)
 			.select('items.title');
 
-		return res.json({ point, items });
+		const serializedPoint = {
+			...point,
+			image_url: `http://192.168.10.8:3333/uploads/${point.image}`,
+		};
+
+		return res.json({ point: serializedPoint, items });
 	}
 
 	async index(req: Request, res: Response) {
@@ -74,7 +81,14 @@ class PointsController {
 			.distinct()
 			.select('points.*');
 
-		return res.json(points);
+		const serializedPoints = points.map((point) => {
+			return {
+				...point,
+				image_url: `http://192.168.10.8:3333/uploads/${point.image}`,
+			};
+		});
+
+		return res.json(serializedPoints);
 	}
 }
 
